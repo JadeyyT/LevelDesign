b22e74428a837d27727dfc97daba91002a188858
kellenstrydom <131270586+kellenstrydom@users.noreply.github.com>
2023-09-19 19:02:57 +0200

    Map

---
+++ Blockout3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 5;
+    private Vector3 input;
+
+    private void Update()
+    {
+        GatherInput();
+        Look();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GatherInput()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    void Look()
+    {
+        if(input != Vector3.zero)
+        {
+            var relative = (transform.position + input) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = rot;
+        }
+        
+    }
+
+    void Move()
+    {
+        rb.MovePosition(transform.position + (transform.forward * input.magnitude) * speed * Time.deltaTime);
+    }
+}
